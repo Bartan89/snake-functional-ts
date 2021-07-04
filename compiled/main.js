984d702fa2be4af28e19da3c98eca05b9e1c49d9
bartKuijper <bart.kuijper@ordina.nl>
2021-07-04 09:27:34 +0200

    init

---
+++ compiled/main.js
@@ -0,0 +1,10 @@
+console.log('works?');
+var Brother = /** @class */ (function () {
+    function Brother(leeftijd) {
+        this.leeftijd = 10;
+        this.leeftijd = leeftijd;
+    }
+    return Brother;
+}());
+console.log(new Brother(1000).leeftijd);
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,14 @@
+console.log('works?')
+
+class Brother {
+  leeftijd: number = 10
+
+  constructor(leeftijd) {
+    this.leeftijd = leeftijd
+  }
+}
+
+
+
+
+console.log(new Brother(1000).leeftijd)
